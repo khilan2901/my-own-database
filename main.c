a0743ec4b0db26b37624398727a5743932622014
shah khilan <shahkhilan2901@gmail.com>
2026-01-12 17:18:08 +0530

    added helping functions

--- main.c
+++ main.c
@@ -117,5 +117,28 @@ int main(int argc, char *argv[])
   {
     print_prompt();
     read_input(input_buffer);
+
+     if (input_buffer->buffer[0] == '.') {
+      switch (do_meta_command(input_buffer)) {
+        case (META_COMMAND_SUCCESS):
+          continue;
+        case (META_COMMAND_UNRECOGNIZED_COMMAND):
+          printf("Unrecognized command '%s'\n", input_buffer->buffer);
+          continue;
+      }
+     }
+
+    Statement statement;
+    switch (prepare_statement(input_buffer, &statement)) {
+      case (PREPARE_SUCCESS):
+        break;
+      case (PREPARE_UNRECOGNIZED_STATEMENT):
+        printf("Unrecognized keyword at start of '%s'.\n",
+               input_buffer->buffer);
+        continue;
+    }
+
+    execute_statement(&statement);
+    printf("Executed.\n");
   }
 }
